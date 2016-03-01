602ef37b8cd039e93e906584fe24cdba3d4c68e2
Jeff Zarnett <jzarnett@gmail.com>
2016-03-01 11:12:38 -0500

    Provide C++ Bindings for Assignment 3

--- Makefile
+++ Makefile
@@ -1,21 +1,32 @@
-CFLAGS?=-Wall -O2 -lm
+RM = rm -f
+
+CC = gcc
+CFLAGS = -O2 -lm
+
+CXX = g++
+CXXFLAGS = -std=c++0x -U__STRICT_ANSI__ -O2 -lOpenCL
 
 default: all
 
-# modify to generate parallel and optimized versions!
-all: build build/nbody-seq
+all: bin nbody-seq nbody report
+
+bin:
+	mkdir bin
+
+nbody-seq: src/nbody-seq.c
+	$(CXX) $< $(CXXFLAGS) -o bin/nbody-seq
+
+nbody: src/nbody.cpp
+	$(CXX) $< $(CXXFLAGS) -o bin/nbody
 
 report: report.pdf
-
-build:
-	mkdir build
-
-build/nbody-seq: src/nbody-seq.c
-	$(CC) $(CFLAGS) -o build/nbody-seq $<
 
 report.pdf: report/report.tex
 	cd report && pdflatex report.tex && pdflatex report.tex
 	mv report/report.pdf report.pdf
 
 clean:
-	rm -r bin
+	$(RM) nbody-seq nbody
+	$(RM) report/*.aux report/*.log
+
+.PHONY: all report clean
---
+++ src/nbody.cpp
@@ -0,0 +1,64 @@
+#define __CL_ENABLE_EXCEPTIONS
+
+#include <CL/cl.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <utility>
+#include <vector>
+ 
+int main() {
+    try { 
+        // Get available platforms
+        std::vector<cl::Platform> platforms;
+        cl::Platform::get(&platforms);
+
+        // Select the default platform and create a context using this platform and the GPU
+        cl_context_properties cps[3] = { 
+            CL_CONTEXT_PLATFORM, 
+            (cl_context_properties)(platforms[0])(), 
+            0 
+        };
+        cl::Context context(CL_DEVICE_TYPE_GPU, cps);
+ 
+        // Get a list of devices on this platform
+        std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
+ 
+        // Create a command queue and use the first device
+        cl::CommandQueue queue = cl::CommandQueue(context, devices[0]);
+ 
+        // Read source file
+        std::ifstream sourceFile("nbody_kernel.cl");
+        std::string sourceCode(std::istreambuf_iterator<char>(sourceFile), (std::istreambuf_iterator<char>()));
+        cl::Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length()+1));
+ 
+        // Make program of the source code in the context
+        cl::Program program = cl::Program(context, source);
+ 
+        // Build program for these specific devices
+        try {
+            program.build(devices);
+        } catch(cl::Error error) {
+            std::cerr << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[0]) << std::endl;
+            throw;
+        }
+ 
+        // Make kernel
+        cl::Kernel kernel(program, "nbody");
+ 
+        // Create buffers
+ 
+        // Write buffers
+
+        // Set arguments to kernel
+
+        // Run the kernel on specific ND range
+ 
+        // Read buffer(s)
+    } catch(cl::Error error) {
+        std::cout << error.what() << "(" << error.err() << ")" << std::endl;
+    }
+ 
+    return EXIT_SUCCESS;
+}
