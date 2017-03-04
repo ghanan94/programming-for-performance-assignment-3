03e45a756d61cc74550e051c1500805d7326bca5
Ghanan Gowripalan <ghanan94@gmail.com>
2017-03-03 20:47:59 -0500

    use the gpu to do nbody-seq calculations

--- src/nbody.cpp
+++ src/nbody.cpp
@@ -7,62 +7,189 @@
 #include <string>
 #include <utility>
 #include <vector>
- 
+#include <random>
+
+#define POINTS (500 * 64)
+#define SPACE (1000.0f)
+
+#define ASSERT(x, str, ...) \
+{ \
+    if ((x) == 0) \
+    { \
+        printf(str, ##__VA_ARGS__); \
+    } \
+}
+
+cl_float4 * initializePositions ()
+{
+    int i;
+    cl_float4 * pts;
+
+    //
+    // For deterministic results;
+    //
+    srand(42L);
+
+    pts = (cl_float4 *) malloc(sizeof(cl_float4) * POINTS);
+
+    for (i = 0; i < POINTS; ++i)
+    {
+        pts[i].x = (((float) std::rand()) / RAND_MAX) * SPACE;
+        pts[i].y = (((float) std::rand()) / RAND_MAX) * SPACE;
+        pts[i].z = (((float) std::rand()) / RAND_MAX) * SPACE;
+
+        //
+        // Size is 1.0f for simplicity.
+        //
+        pts[i].w = 1.0f;
+    }
+
+    return pts;
+}
+
+cl_float4 * initializeAccelerations ()
+{
+    int i;
+    cl_float4 * pts;
+
+    /*
+    pts = (cl_float4 *) malloc(sizeof(cl_float4) * POINTS);
+
+    for (i = 0; i < POINTS; ++i)
+    {
+        pts[i].x = 0;
+        pts[i].y = 0;
+        pts[i].z = 0;
+        pts[i].w = 0;
+    }
+    */
+
+    pts = (cl_float4 *) calloc(POINTS, sizeof(cl_float4));
+
+    return pts;
+}
+
+
 int main() {
-    try { 
-        // Get available platforms
-        std::vector<cl::Platform> platforms;
-        cl::Platform::get(&platforms);
+    try {
+    // Get available platforms
+    std::vector<cl::Platform> platforms;
 
-        // Select the default platform and create a context using this platform and the GPU
-        cl_context_properties cps[3] = { 
-            CL_CONTEXT_PLATFORM, 
-            (cl_context_properties)(platforms[0])(), 
-            0 
-        };
-        cl::Context context(CL_DEVICE_TYPE_GPU, cps);
- 
-        // Get a list of devices on this platform
-        std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
- 
-        // Create a command queue and use the first device
-        cl::CommandQueue queue = cl::CommandQueue(context, devices[0]);
- 
-        // Read source file
-        std::ifstream sourceFile("nbody_kernel.cl");
-            if(!sourceFile.is_open()){
-                std::cerr << "Cannot find kernel file" << std::endl;
-                throw;
-            }
-        std::string sourceCode(std::istreambuf_iterator<char>(sourceFile), (std::istreambuf_iterator<char>()));
-        cl::Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length()+1));
- 
-        // Make program of the source code in the context
-        cl::Program program = cl::Program(context, source);
- 
-        // Build program for these specific devices
-        try {
-            program.build(devices);
-        } catch(cl::Error error) {
-            std::cerr << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[0]) << std::endl;
-            throw;
-        }
- 
-        // Make kernel
-        cl::Kernel kernel(program, "nbody");
- 
-        // Create buffers
- 
-        // Write buffers
+    cl::Platform::get(&platforms);
 
-        // Set arguments to kernel
+    // Select the default platform and create a context using this platform and the GPU
+    cl_context_properties cps[3] = {
+        CL_CONTEXT_PLATFORM,
+        (cl_context_properties)(platforms[0])(),
+        0
+    };
+    cl::Context context(CL_DEVICE_TYPE_GPU, cps);
 
-        // Run the kernel on specific ND range
- 
-        // Read buffer(s)
+    // Get a list of devices on this platform
+    std::vector<cl::Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
+
+    // Create a command queue and use the first device
+    cl::CommandQueue queue = cl::CommandQueue(context, devices[0]);
+
+    // Read source file
+    std::ifstream sourceFile("src/nbody_kernel.cl");
+
+    if(!sourceFile.is_open()) {
+        std::cerr << "Cannot find kernel file" << std::endl;
+        throw;
+    }
+
+    std::string sourceCode(std::istreambuf_iterator<char>(sourceFile), (std::istreambuf_iterator<char>()));
+    cl::Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length() + 1));
+
+    // Make program of the source code in the context
+    cl::Program program = cl::Program(context, source);
+
+    // Build program for these specific devices
+    try {
+        program.build(devices);
+    } catch(cl::Error error) {
+        std::cerr << program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[0]) << std::endl;
+        throw;
+    }
+
+    // Make kernel
+    cl::Kernel kernel(program, "nbody");
+
+    // Create buffers
+    cl_int err = 0;
+
+    printf("Create buffers\n");
+    cl_float4 * x = initializePositions();
+    cl_float4 * a = initializeAccelerations();
+    int points = POINTS;
+
+    //
+    // Buffer for positions array
+    //
+    cl::Buffer x_buffer(context, CL_MEM_READ_WRITE, POINTS * sizeof(cl_float4), &err);
+    ASSERT(err == CL_SUCCESS, "err was %d\n", err);
+
+    //
+    // Buffer for acceleration array
+    //
+    cl::Buffer a_buffer(context, CL_MEM_READ_WRITE, POINTS * sizeof(cl_float4), &err);
+    ASSERT(err == CL_SUCCESS, "err was %d\n", err);
+
+    //
+    // Buffer for points value (need to have all values in a buffer)
+    //
+    cl::Buffer points_buffer(context, CL_MEM_READ_ONLY, sizeof(int), &err);
+    ASSERT(err == CL_SUCCESS, "err was %d\n", err);
+
+    // Write buffers
+    printf("Write buffers\n");
+    err = queue.enqueueWriteBuffer(x_buffer, CL_TRUE, 0, POINTS * sizeof(cl_float4), x);
+    ASSERT(err == CL_SUCCESS, "err was %d\n", err);
+
+    err = queue.enqueueWriteBuffer(a_buffer, CL_TRUE, 0, POINTS * sizeof(cl_float4), a);
+    ASSERT(err == CL_SUCCESS, "err was %d\n", err);
+
+    err = queue.enqueueWriteBuffer(points_buffer,  CL_TRUE, 0, sizeof(int), &points);
+    ASSERT(err == CL_SUCCESS, "err was %d\n", err);
+
+    // Set arguments to kernel
+    printf("Set args\n");
+    err = kernel.setArg(0, x_buffer);
+    ASSERT(err == CL_SUCCESS, "err was %d\n", err);
+
+    err = kernel.setArg(1, a_buffer);
+    ASSERT(err == CL_SUCCESS, "err was %d\n", err);
+
+    err = kernel.setArg(2, points_buffer);
+    ASSERT(err == CL_SUCCESS, "err was %d\n", err);
+
+    // Run the kernel on specific ND range
+    printf("Run\n");
+    err = queue.enqueueNDRangeKernel(kernel, cl::NDRange(0), cl::NDRange(POINTS), cl::NullRange);
+    ASSERT(err == CL_SUCCESS, "err was %d\n", err);
+
+    // Read buffer(s)
+    printf("Read buffers\n");
+    err = queue.enqueueReadBuffer(x_buffer, CL_TRUE, 0, POINTS * sizeof(cl_float4), x);
+    ASSERT(err == CL_SUCCESS, "err was %d\n", err);
+
+    err = queue.enqueueReadBuffer(a_buffer, CL_TRUE, 0, POINTS * sizeof(cl_float4), a);
+    ASSERT(err == CL_SUCCESS, "err was %d\n", err);
+
+    for (int i = 0; i < POINTS; ++i)
+    {
+        printf("(%2.2f,%2.2f,%2.2f,%2.2f) (%2.3f,%2.3f,%2.3f)\n",
+           x[i].x, x[i].y, x[i].z, x[i].w,
+           a[i].x, a[i].y, a[i].z);
+    }
+
+    free(x);
+    free(a);
+
     } catch(cl::Error error) {
         std::cout << error.what() << "(" << error.err() << ")" << std::endl;
     }
- 
+
     return EXIT_SUCCESS;
 }
--- src/nbody_kernel.cl
+++ src/nbody_kernel.cl
@@ -1,4 +1,53 @@
-__kernel void nbody()
-{
+#define EPS (1e-10)
 
+inline void body_body_interaction (
+    float4 bi,
+    float4 bj,
+    float4 * ai
+    )
+{
+    float4 r;
+    float dist_sqr;
+    float dist_sixth;
+    float inv_dist_cube;
+    float s;
+
+
+    r.x = bj.x - bi.x;
+    r.y = bj.y - bi.y;
+    r.z = bj.z - bi.z;
+    r.w = 1.0f;
+
+    dist_sqr = r.x * r.x + r.y * r.y + r.z * r.z + EPS;
+
+    dist_sixth = dist_sqr * dist_sqr * dist_sqr;
+    inv_dist_cube = 1.0f / sqrt(dist_sixth);
+
+    s = bj.w * inv_dist_cube;
+
+    ai->x += r.x * s;
+    ai->y += r.y * s;
+    ai->z += r.z * s;
+}
+
+__kernel void nbody (
+    global float4 * global_p,
+    global float4 * global_a,
+    global const int * points
+    )
+{
+    int global_id;
+    int i;
+    float4 my_position;
+    float4 acc;
+
+    global_id = get_global_id(0);
+    my_position = global_p[global_id];
+
+    for (i = 0; i < points[0]; ++i)
+    {
+        body_body_interaction(my_position, global_p[i], &acc);
+    }
+
+    global_a[global_id] = acc;
 }
